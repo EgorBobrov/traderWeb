5164a9c414ab5ea566c744635983bd9872578dee
EgorBobrov <egorbobrov1776@gmail.com>
2016-02-22 18:17:51 +0300

    Version 0.2, now actually playable

---
+++ script.js
@@ -0,0 +1,225 @@
+function query (slaves, cotton, guns) {
+	this.slaves = slaves;
+	this.cotton = cotton;
+	this.guns = guns;
+}
+
+function randMult(rand, slMul, cotMul, gunMul) {
+    this.rand = Math.floor(Math.random() * 10);
+    this.slMul = 0.5 + Math.random();
+    this.cotMul = 0.5 + Math.random();
+    this.gunMul = 0.5 + Math.random();
+}
+
+function alerts (sp, cp, gp) {
+    message = "Price fluctuations: ";
+    noFluc = true;
+    if (sp > 1.2*(300000 / slaves)) {
+        message += "Slaves prices are rising. "
+        noFluc = false;
+    };
+    if (sp < 0.8*(300000 / slaves)) {
+        message += "Slaves prices are dropping. "
+        noFluc = false;
+    };
+    if (cp > 1.2*(300000 / cotton)) {
+        message += "Cotton prices are rising. "
+        noFluc = false;
+    };
+    if (cp < 0.8*(300000 / cotton)) {
+        message += "Cotton prices are dropping. "
+        noFluc = false;
+    };
+
+    if (gp > 1.2*(200000 / guns)) {
+        message += "Guns prices are rising. ";
+        noFluc = false;
+    };
+    if (gp < 0.8*(200000 / guns)) {
+        message += "Guns prices are dropping.";
+        noFluc = false;
+    };
+    if(noFluc) {
+        message += "Nothing significant.";
+    };
+
+    window.alert(message);
+}
+
+var x = 1000;
+document.getElementById("cash").innerHTML = x + '$';
+
+var slaves = 500;
+var mySlaves = 0;
+var slavesPrice = 300000 / slaves;
+document.getElementById("slavesMarket").innerHTML = slaves;
+document.getElementById("slavesStorage").innerHTML = mySlaves;
+document.getElementById("slavesPrice").innerHTML = slavesPrice + '$';
+document.getElementById("slavePrice").innerHTML = slavesPrice + '$';
+
+var cotton = 10000;
+var myCotton = 0;
+var cottonPrice = 300000 / cotton;
+document.getElementById("cottonMarket").innerHTML = cotton;
+document.getElementById("cottonStorage").innerHTML = myCotton;
+document.getElementById("cottonPrice").innerHTML = cottonPrice + '$';
+document.getElementById("cottoPrice").innerHTML = cottonPrice + '$';
+
+var guns = 4000;
+var myGuns = 0;
+var gunsPrice = 200000 / guns;
+document.getElementById("gunsMarket").innerHTML = guns;
+document.getElementById("gunsStorage").innerHTML = myGuns;
+document.getElementById("gunsPrice").innerHTML = gunsPrice + '$';
+document.getElementById("gunPrice").innerHTML = gunsPrice + '$';
+
+function tradeStuff(a) {
+	var tradeQuery; //= new query(0, 0, 0);
+	if (a == 1) {
+		tradeQuery = new query(document.getElementById('buySlaves').value, document.getElementById('buyCotton').value, document.getElementById('buyGuns').value);
+        if (tradeQuery.slaves < 0 || tradeQuery.cotton < 0 || tradeQuery.guns < 0) {
+            window.alert("You can't buy negative amount of goods!");
+            return;
+        };
+
+        if (slaves >= tradeQuery.slaves && cotton >= tradeQuery.cotton && guns >= tradeQuery.guns) {
+            slaves = slaves - a*tradeQuery.slaves;
+            mySlaves = mySlaves -  a*-tradeQuery.slaves;
+            cotton = cotton - a*tradeQuery.cotton;
+            myCotton = myCotton - a*-tradeQuery.cotton;
+            guns = guns - a*tradeQuery.guns;
+            myGuns = myGuns - a*-tradeQuery.guns;
+        }
+
+        else {
+            window.alert ("You are trying to buy more goods than available for sale!")
+            return;
+        };
+        
+	}
+	else if (a == -1){
+		tradeQuery = new query(document.getElementById('sellSlaves').value, document.getElementById('sellCotton').value, document.getElementById('sellGuns').value);
+        if (tradeQuery.slaves < 0 || tradeQuery.cotton < 0 || tradeQuery.guns < 0) {
+            window.alert("You can't sell negative amount of goods!");
+            return;
+        };
+
+        if (mySlaves >= tradeQuery.slaves && myCotton >= tradeQuery.cotton && myGuns >= tradeQuery.guns) {
+            slaves = slaves - a*tradeQuery.slaves;
+            mySlaves = mySlaves -  a*-tradeQuery.slaves;
+            cotton = cotton - a*tradeQuery.cotton;
+            myCotton = myCotton - a*-tradeQuery.cotton;
+            guns = guns - a*tradeQuery.guns;
+            myGuns = myGuns - a*-tradeQuery.guns;
+        }
+        else {
+            window.alert ("You are trying to sell more goods than you have for sale!")
+            return;
+        };
+	}
+    
+    document.getElementById("slavesMarket").innerHTML = slaves;
+    document.getElementById("cottonMarket").innerHTML = cotton;
+    document.getElementById("gunsMarket").innerHTML = guns;
+
+    document.getElementById("slavesStorage").innerHTML = mySlaves;
+    document.getElementById("cottonStorage").innerHTML = myCotton;
+    document.getElementById("gunsStorage").innerHTML = myGuns;
+
+    x = x - a*(tradeQuery.slaves * slavesPrice + tradeQuery.cotton * cottonPrice + tradeQuery.guns * gunsPrice);
+    document.getElementById("cash").innerHTML = x + '$';
+
+}
+
+function nextTurn() {
+    if (x < 0) {
+        window.alert("You are a bancrupt!");
+        location.reload();
+    };
+
+    var random = new randMult(0, 0, 0, 0);
+
+    switch(random.rand) {
+        case 0:
+        case 1: 
+            slaves = slaves + Math.floor(Math.random() * 50);
+            slavesPrice = (300000 / slaves)*random.slMul;
+            document.getElementById("slavesPrice").innerHTML = slavesPrice + '$';
+            document.getElementById("slavePrice").innerHTML = slavesPrice + '$';
+            document.getElementById("slavesMarket").innerHTML = slaves;
+            window.alert("Slaves supply is abundant this week. Cotton and gun industry are not doing so well.");
+            break;
+
+        case 2:
+        case 3:
+            cotton = cotton + Math.floor(Math.random() * 300);
+            cottonPrice = (300000 / cotton)*random.cotMul;
+            document.getElementById("cottonPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("cottoPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("cottonMarket").innerHTML = cotton;
+
+            window.alert("Cotton supply is abundant this week. Slaves and gun industry are not doing so well.");
+            break;
+
+        case 4:
+        case 5:
+            guns = guns + Math.floor(Math.random() * 200);
+            gunsPrice = (200000 / guns)*random.gunMul;
+            document.getElementById("gunsPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunsMarket").innerHTML = guns;
+
+            window.alert("Guns supply is abundant this week. Cotton and slaves industry are not doing so well.");
+
+            break;
+
+        case 6:
+        case 7:
+            slaves = slaves + Math.floor(Math.random() * 30);
+            cotton = cotton + Math.floor(Math.random() * 300);
+            guns = guns + Math.floor(Math.random() * 150);
+            slavesPrice = (300000 / slaves)*random.slMul;
+            cottonPrice = (300000 / cotton)*random.cotMul;
+            gunsPrice = (200000 / guns)*random.gunMul;
+            document.getElementById("slavesPrice").innerHTML = slavesPrice + '$';
+            document.getElementById("slavePrice").innerHTML = slavesPrice + '$';
+            document.getElementById("cottonPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("cottoPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("gunsPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunsMarket").innerHTML = guns;
+            document.getElementById("cottonMarket").innerHTML = cotton;
+            document.getElementById("slavesMarket").innerHTML = slaves;
+            window.alert("Supply of all types of goods grows steadily.");
+            break;
+
+
+        case 8:
+        case 9:
+            slaves = slaves - Math.floor(Math.random() * 30);
+            cotton = cotton - Math.floor(Math.random() * 300);
+            guns = guns - Math.floor(Math.random() * 150);
+            slavesPrice = (300000 / slaves)*random.slMul;
+            cottonPrice = (300000 / cotton)*random.cotMul;
+            gunsPrice = (200000 / guns)*random.gunMul;
+            document.getElementById("slavesPrice").innerHTML = slavesPrice + '$';
+            document.getElementById("slavePrice").innerHTML = slavesPrice + '$';
+            document.getElementById("cottonPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("cottoPrice").innerHTML = cottonPrice + '$';
+            document.getElementById("gunsPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunPrice").innerHTML = gunsPrice + '$';
+            document.getElementById("gunsMarket").innerHTML = guns;
+            document.getElementById("cottonMarket").innerHTML = cotton;
+            document.getElementById("slavesMarket").innerHTML = slaves;
+
+            window.alert("Supply of all types of goods drops.");
+            break;
+    }
+
+    var al = new alerts(slavesPrice, cottonPrice, gunsPrice);
+    var tax = x * 0.13;
+    x = x - tax;
+    document.getElementById("cash").innerHTML = x + '$';
+
+}
+
